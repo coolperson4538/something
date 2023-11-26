ee59d0f5fb8e1179466ed52a87fffe4ad50e2955
coolperson4538 <147566059+coolperson4538@users.noreply.github.com>
2023-11-26 23:14:09 +0000

    Apex legend cheat

--- main.cpp
+++ main.cpp
@@ -1,1 +1,88 @@
+#include "misc.hpp"
 
+using namespace std; // So we dont have to write bloated namespaces
+namespace Offsets // games offsets
+{
+    constexpr string local_player = 0x1dacd48; // The offset for local player
+    constexpr string glow_enable = 0x10f; // enables glowing (chams, esp etc etc)
+    constexpr string view_matrix = 0x602f41; // View Matrix for campos (I think)
+    constexpr string glow_type = 0x2c0; // type of glow or whatever
+}
+
+namespace Cheats
+{
+    namespace Menu
+    {
+        ImGui::Begin("Apex Legends Cheat.") // Windows title
+
+        if(ImGui::Button("Chams")) // Creates a button called chams.
+        {
+            // Work in progress
+        }
+
+        if(ImGui::Button("Fly"))
+        {
+            // Work in progress
+        }
+
+        if(ImGui::Button("Noclip"))
+        {
+            // Work in progress
+        }
+
+        if(ImGui::Button("Aimbot"))
+        {
+            // Work in progress
+        }
+    }
+
+    namespace Cheat
+    {
+        void chams()
+        {
+
+        }
+
+        void fly()
+        {
+
+        }
+
+        void noclip()
+        {
+
+        }
+    }
+}
+
+/*
+
+client_render_targets	0x1fdf3a0	
+ibase_client_dll	0x19e1af0	
+iclient_entity_list	0x1fdebb0	
+cprediction	0x1fdebb8	
+iv_model_render	0x128b080	
+vgui_system	0x1f98b00	
+iv_render_view	0x7519c18	
+iv_debug_overlay	0x75b4da0	
+iv_model_info_client	0x7519c28	
+iv_engine_client	0x12799d8	
+iv_engine_trace	0x7519c30	
+inetwork_string_table	0x7519c40	
+cnetwork_u64_table	0x7519c48	
+cinput	0x1d95220	
+cengine	0x1292410	
+iswapchain	0xb912398	
+cl_entitylist	0x19fbf98	
+local_entity_handle	0x127d8bc	
+local_player	0x1dacd48	
+global_vars	0x135c910	
+name_list	0xb9f8340	
+view_render	0x1fe04b0	
+view_matrix	0x602f41	
+client_state	0x135cc90	
+sign_on_state	0x135cd28	
+level_name	0x135ce40	
+glow_enable	0x10f	
+glow_type	0x2c0	
+glow_color	0x1d0*/
