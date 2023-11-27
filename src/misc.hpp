2546edb92ea6b1e5aa39fd898ef8b99f7afee3c2
coolperson4538 <147566059+coolperson4538@users.noreply.github.com>
2023-11-27 03:27:17 +0000

    dasdasd

--- src/misc.hpp
+++ src/misc.hpp
@@ -4,7 +4,13 @@
 #include <vector> // for arrays like std::vector<>
 #include <cstdint> // for basically like 32 bit ints and 64bit data types like int32_t
 #include <mutex>
+#include <d3d9.h>
 #include "imgui.h" // for menu
 #include "driver.h" // for kernel driver (bypassing anticheat)
 #include "memory.h" // memory management shit
-#include "sdk.h" // procid
+#include "sdk.h" // proc id
+#include "modules/ESP.hpp"
+#include "modules/Fly.hpp"
+#include "modules/NoClip.hpp"
+
+#pragma comment(lib, "d3d9.lib")
